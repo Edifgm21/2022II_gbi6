8c239f1de595d65f2afec79a6978f836c3529b72
Gonzales_edi_biotech <100884674+Edifgm21@users.noreply.github.com>
2022-11-09 10:02:12 -0500

    codigos de forma de mostrar las primeras y ultimas filas

---
+++ TC2/tc2_GonzalesEdison.sh
@@ -0,0 +1,14 @@
+#!Bin Bash
+
+echo "primeras 7 filas" # escribimos echo para que me indique que se imprimio las 7 primerass filas 
+
+
+head -n 7 EigenBetw.tsv # uso el comando cut para que muestre las primeras 7 filas
+
+
+echo "ultimas 5 filas" # uso el comando echo para que me muestre qye se imprimio las 5 ultimas filas 
+
+
+tail -n 5 EigenBetw.tsv # uso el comando tail para que me  imprima las 5 ultimas filas 
+
+
